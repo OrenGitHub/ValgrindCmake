e90232e1f2ebb75f79b10fd0af7a6ed02fda284a
OrenGitHub <tuna.is.good.for.you@gmail.com>
2022-11-05 17:04:38 +0200

    treat leaks as errors - finally

--- src/main.cpp
+++ src/main.cpp
@@ -1,15 +1,19 @@
 #include <iostream>
 
 class Address { public:
-    std::string street;
+    const char *street;
     int building;
-    Address(const std::string &s):street(s), building(6){}
+    Address(const char *s):street(s), building(6){}
 };
+
+class Person { public: Address *address; };
 
 int main(int argc, char **argv)
 {
     Address *address = new Address("5thAvenue");
     address->building = 20;
+    Person *person = new Person();
+    person->address = address;
     std::cout << "OK PERSON\n";
     return 0;
 }
